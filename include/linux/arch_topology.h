8bb92e900ee98ad025162139f18109b95aa6931e
Thara Gopinath <thara.gopinath@linaro.org>
2020-02-21 19:52:07 -0500

    drivers/base/arch_topology: Add infrastructure to store and update instantaneous thermal pressure
    Add architecture specific APIs to update and track thermal pressure on a
    per CPU basis. A per CPU variable thermal_pressure is introduced to keep
    track of instantaneous per CPU thermal pressure. Thermal pressure is the
    delta between maximum capacity and capped capacity due to a thermal event.
    
    topology_get_thermal_pressure can be hooked into the scheduler specified
    arch_scale_thermal_pressure to retrieve instantaneous thermal pressure of
    a CPU.
    
    arch_set_thermal_pressure can be used to update the thermal pressure.
    
    Considering topology_get_thermal_pressure reads thermal_pressure and
    arch_set_thermal_pressure writes into thermal_pressure, one can argue for
    some sort of locking mechanism to avoid a stale value.  But considering
    topology_get_thermal_pressure can be called from a system critical path
    like scheduler tick function, a locking mechanism is not ideal. This means
    that it is possible the thermal_pressure value used to calculate average
    thermal pressure for a CPU can be stale for up to 1 tick period.
    
    Signed-off-by: Thara Gopinath <thara.gopinath@linaro.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Link: https://lkml.kernel.org/r/20200222005213.3873-4-thara.gopinath@linaro.org

--- include/linux/arch_topology.h
+++ include/linux/arch_topology.h
@@ -34,12 +34,21 @@ unsigned long topology_get_freq_scale(int cpu)
 }
 
 DECLARE_PER_CPU(unsigned long, max_freq_scale);
+DECLARE_PER_CPU(unsigned long, thermal_pressure);
 
 static inline
 unsigned long topology_get_max_freq_scale(struct sched_domain *sd, int cpu)
 {
 	return per_cpu(max_freq_scale, cpu);
 }
+
+static inline unsigned long topology_get_thermal_pressure(int cpu)
+{
+	return per_cpu(thermal_pressure, cpu);
+}
+
+void arch_set_thermal_pressure(struct cpumask *cpus,
+			       unsigned long th_pressure);
 
 struct cpu_topology {
 	int thread_id;
