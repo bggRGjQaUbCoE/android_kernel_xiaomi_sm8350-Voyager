d27021567f45045431472c5ae47e1efaf4f88ed6
tanz3 <3222839916@qq.com>
2023-01-14 14:00:20 +0800

    drivers: android: introduce PERF_CRITICAL_RT_TASK * From redwood-s-oss
    Signed-off-by: tanz3 <3222839916@qq.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -2464,6 +2464,10 @@ long _do_fork(struct kernel_clone_args *args)
 		task_unlock(p);
 	}
 
+#ifdef CONFIG_PERF_CRITICAL_RT_TASK
+	p->critical_rt_task = 0;
+#endif
+
 #ifdef CONFIG_SF_BINDER
 	p->sf_binder_task = 0;
 #endif
